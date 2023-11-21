a00aef095bd1f813e06808ca1a7831fe156af1fb
dana096 <carpediem096@naver.com>
2023-11-21 22:10:03 +0900

    day01.Test01.update

--- JavaProject/src/Project01/Test01.java
+++ JavaProject/src/Project01/Test01.java
@@ -2,7 +2,8 @@ package Project01;
 
 public class Test01 {
 	public static void main(String[] args) {
-		System.out.print("Hellow World!");
+		System.out.println("Hellow World!");
+		System.out.println(100);
 	}
 
 }
